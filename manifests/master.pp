c511e1053e05c27d8a52f483a3efc641172767cb
Walter Heck <walter@tribily.com>
2012-03-08 10:05:36 +0800

    Add puppet_server parameter so that puppet.conf-common.erb can pick up the right value

--- manifests/master.pp
+++ manifests/master.pp
@@ -58,29 +58,29 @@
 #  }
 #
 class puppet::master (
-  $modulepath = $::puppet::params::modulepath,
-  $confdir = $::puppet::params::confdir,
-  $manifest = $::puppet::params::manifest,
-  $storeconfigs = false,
-  $storeconfigs_dbadapter = $::puppet::params::storeconfigs_dbadapter,
-  $storeconfigs_dbuser = $::puppet::params::storeconfigs_dbuser,
-  $storeconfigs_dbpassword = $::puppet::params::storeconfigs_dbpassword,
-  $storeconfigs_dbserver = $::puppet::params::storeconfigs_dbserver,
-  $storeconfigs_dbsocket = $::puppet::params::storeconfigs_dbsocket,
-  $install_mysql_pkgs = $::puppet::params::puppet_storeconfigs_packages,
-  $certname = $::fqdn,
-  $autosign = false,
-  $dashboard_port = 3000,
-  $puppet_passenger = false,
-  $puppet_site = $::puppet::params::puppet_site,
-  $puppet_docroot = $::puppet::params::puppet_docroot,
-  $puppet_vardir = $::puppet::params::puppet_vardir,
-  $puppet_passenger_port = false,
-  $puppet_master_package = $::puppet::params::puppet_master_package,
-  $package_provider = undef,
-  $puppet_master_service = $::puppet::params::puppet_master_service,
-  $version = 'present'
-
+  $modulepath               = $::puppet::params::modulepath,
+  $confdir                  = $::puppet::params::confdir,
+  $manifest                 = $::puppet::params::manifest,
+  $storeconfigs             = false,
+  $storeconfigs_dbadapter   = $::puppet::params::storeconfigs_dbadapter,
+  $storeconfigs_dbuser      = $::puppet::params::storeconfigs_dbuser,
+  $storeconfigs_dbpassword  = $::puppet::params::storeconfigs_dbpassword,
+  $storeconfigs_dbserver    = $::puppet::params::storeconfigs_dbserver,
+  $storeconfigs_dbsocket    = $::puppet::params::storeconfigs_dbsocket,
+  $install_mysql_pkgs       = $::puppet::params::puppet_storeconfigs_packages,
+  $certname                 = $::fqdn,
+  $autosign                 = false,
+  $dashboard_port           = 3000,
+  $puppet_passenger         = false,
+  $puppet_site              = $::puppet::params::puppet_site,
+  $puppet_docroot           = $::puppet::params::puppet_docroot,
+  $puppet_vardir            = $::puppet::params::puppet_vardir,
+  $puppet_passenger_port    = false,
+  $puppet_master_package    = $::puppet::params::puppet_master_package,
+  $package_provider         = undef,
+  $puppet_master_service    = $::puppet::params::puppet_master_service,
+  $version                  = 'present',
+  $puppet_server            = $puppet::params::puppet_server
 ) inherits puppet::params {
 
   include concat::setup
